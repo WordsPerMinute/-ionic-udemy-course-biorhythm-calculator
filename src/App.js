3be98f7efe23dfa12cd3c29dd0fedd4f31a53220
WordsPerMinute <git@coreyhodge.net>
2020-07-19 16:20:59 -0600

    finished with enough course material to start on own app

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ import {
 } from '@ionic/react';
 import React, { useState } from 'react';
 import BiorhythmCard from './components/BiorhythmCard'
+import { useLocalStorage } from './hooks'
 
 function App() {
-  const [birthDate, setBirthDate] = useState('');
+  const [birthDate, setBirthDate] = useLocalStorage('birthDate', '')
   const [targetDate, setTargetDate] = useState(new Date().toISOString());
-
 
   return (
     <IonApp>
